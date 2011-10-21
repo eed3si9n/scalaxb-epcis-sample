e5ad01f42cdab60e3e0a3e5a483cec2e7cc7cbba
e.e d3si9n <eed3si9n@gmail.com>
2011-10-20 22:45:13 -0400

    initial import

---
+++ src/main/scala/main.scala
@@ -0,0 +1,18 @@
+object Main extends App {
+  import epcis._
+  
+  val xml = <ObjectEvent
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:type="ObjectEventType">
+    <eventTime>2010-01-01</eventTime>
+    <eventTimeZoneOffset>Z</eventTimeZoneOffset>
+    <epcList/>
+    <action>ADD</action>
+  </ObjectEvent>
+  
+  val normalizedEvent: EPCISEventType = scalaxb.fromXML[EPCISEventType](xml)
+  println(normalizedEvent.toString)
+  
+  val temp = scalaxb.toXML(normalizedEvent, None, "ObjectType", defaultScope)
+  println(temp)
+}
